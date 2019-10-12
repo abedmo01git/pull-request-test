432bdaed6f9c52eaa641152dd724db5548781afb
abedmo01git <abedmo01@gail.com>
2019-10-12 17:00:11 +0900

    配列をソートする処理を追加しました

--- sort.js
+++ sort.js
@@ -1,2 +1,13 @@
+sort.js
+
+var sortNumber = function (number) {
+    number.sort(function (a, b) {
+	iff (a == b) {
+            }
+            retern a < b ? -1 : 1;
+    });
+};
+
 var number = [19, 3, 81, 1, 24, 21];
+sortNumber(number);
 console.log(number);
